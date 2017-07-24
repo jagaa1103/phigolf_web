5ed579c0f1a9362ae74515e084cc296913b5bb0c
jagaa <jagaa1103@gmail.com>
2017-07-24 20:25:13 +0900

    fixing menu bar colors

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['duScroll']);
 
-app.controller('mainCtrl', function($scope){
+app.controller('mainCtrl', function($scope, $timeout, $document, $location){
     console.log("mainCtrl");
-    
+    $scope.menu_icon = {phigolf : 'focus', overview: 'nor_white', features: 'nor_white', support: 'nor_white', store: 'nor_white'};
+
+    function init(){
+        initMenu('phigolf');
+    }
+    init();
+
+    $scope.selectedView = function(page){
+        initMenu(page);
+    }
+
+    function initMenu(page){
+        if(page === "overviewView"){
+            $scope.menu_icon = {phigolf : 'nor_black', overview: 'focus', features: 'nor_black', support: 'nor_black', store: 'nor_black'};
+        }else if(page === "featuresView"){
+            $scope.menu_icon = {phigolf : 'nor_black', overview: 'nor_black', features: 'focus', support: 'nor_black', store: 'nor_black'};
+        }else if(page === "supportView"){
+            $scope.menu_icon = {phigolf : 'nor_black', overview: 'nor_black', features: 'nor_black', support: 'focus', store: 'nor_black'};
+        }else if(page === "storeView"){
+            $scope.menu_icon = {phigolf : 'nor_black', overview: 'nor_black', features: 'nor_black', support: 'nor_black', store: 'focus'};
+        } else {
+            $scope.menu_icon = {phigolf : 'focus', overview: 'nor_white', features: 'nor_white', support: 'nor_white', store: 'nor_white'};
+        }
+
+        if(page === "phigolf"){
+            document.getElementById('main_top_menu_bar').style.backgroundColor = "";
+            $document.scrollTop(0, 200).then(function() {});
+        }else{
+            document.getElementById('main_top_menu_bar').style.backgroundColor = "#ffffff";
+            var someElement = angular.element(document.getElementById(page));
+            $document.scrollToElement(someElement, 0, 200);
+        }
+        
+    }
 });
